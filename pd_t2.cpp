525b1cca389915212255f7a5c94e9a4c15fa8f36
Muntaha Fatima <immuntahafatima@gmail.com>
2024-09-23 13:14:42 +0500

    Update pd_t2.cpp

--- pd_t2.cpp
+++ pd_t2.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 main()
 {
-	int n,f,t;
+	int minutes,frames,total;
 	cout<<"Number of Minutes: ";
-	cin>>n;
+	cin>>minutes;
 	cout<<"Frames per Second: ";
-	cin>>f;
+	cin>>frames;
 	//we multiply n with 60 to convert it into seconds
-	t=f*(n*60);
-	cout<<"Total Number of Frames: "<<t<<endl;
+	total=frames*(minutes*60);
+	cout<<"Total Number of Frames: "<<total<<endl;
 }
